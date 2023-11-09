4e169f7976507d58c8aafeb65df14cabc1742b63
Britain <britaing@gmail.com>
2023-11-09 14:01:53 -0600

    filled out about section

--- src/components/about/index.jsx
+++ src/components/about/index.jsx
@@ -1,9 +1,10 @@
-import { Figure, Row, Col } from "react-bootstrap";
+import { Figure, Row, Col, Container } from "react-bootstrap";
 import profilePic from "../../assets/profileImage.jpg";
 function About() {
   return (
     <>
-      <Row>
+    <Container >
+      <Row >
         <Col>
           <Figure>
             <Figure.Image
@@ -17,11 +18,22 @@ function About() {
         <Col>
           <Figure>
             <Figure.Caption>
-              Nulla vitae elit libero, a pharetra augue mollis interdum.
+              My name is Britain Gilgour and I have recently began my journey into coding. So far coding has been
+              incredibly fast paced, challenging, and at times incredibly frustrating...I love it.
+              <br/>
+              I got into coding on a whim but found that I really enjoyed the challenge of it and decided to go for a career
+              change. 
+              <br/>
+              When I'm not studying coding I am spending time with my supportive wife and my three delightful cats,
+              playing video games, or, working at a restaurant as a floor manager.
+              <br/>
+              This site highlights some of my work so far and is (and probably will remain) a work in progress. 
+              
             </Figure.Caption>
           </Figure>
         </Col>
       </Row>
+      </Container>
     </>
   );
 }
--- src/components/footer/index.jsx
+++ src/components/footer/index.jsx
@@ -1,13 +1,11 @@
-import React from 'react';
+import React from "react";
 
 function Footer() {
-
-    return (
-        <div>
-            <p>2023 Britain Gilgour Bootcamp</p>
-        </div>
-    );
-
+  return (
+    <div>
+      <p>2023 Britain Gilgour Bootcamp</p>
+    </div>
+  );
 }
 
 export default Footer;
